009bd81df58b3d25db20de304dd9a13c0e9b0e19
Yetong Pang <404060776@qq.com>
2022-10-24 13:29:57 +0800

    FirstPart

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import re
+import urllib.request,urllib.error
+import bs4
+
+baseUrl = "https://www.luogu.com.cn/problem/P"
+minn = 1000
+maxn = 8596
+savePath = "D:\\programming documents\\python\\programs\\DownloadLuoguProblems\\problems\\"
+
+def main():
+    print("计划爬取到P{}".format(maxn))
+    for i in range(minn,maxn+1):
+        print("正在爬取P{}...".format(i),end="")
+        html = getHTML(baseUrl +str(i))
+        if html == "error":
+            print("爬取失败，可能是不存在该题或无权查看")
+        else:
+            problemMD = getMD(html)
+            print("爬取成功！正在保存...",end="")
+            saveData(problemMD,"P"+str(i)+".md")
+            print("保存成功！")
+    print("爬取完毕")
+
+def getHTML(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+    }
+    request = urllib.request.Request(url = url,headers = headers)
+    response = urllib.request.urlopen(request)
+    html = response.read().decode('utf-8')
+    if str(html).find("Exception") == -1:
+        return html
+    else:
+        return "error"
+
+def getMD(html):
+    bs = bs4.BeautifulSoup(html,"html.parser")
+    core = bs.select("article")[0]
+    md = str(core)
+    md = re.sub("<h1>","# ",md)
+    md = re.sub("<h2>","## ",md)
+    md = re.sub("<h3>","### ",md)
+    md = re.sub("</?[a-zA-Z]+[^<>]*>","",md)
+    return md
+
+def saveData(data,filename):
+    cfilename = savePath + filename
+    file = open(cfilename,"w",encoding="utf-8")
+    for d in data:
+        file.writelines(d)
+    file.close()
+
+if __name__ == '__main__':
+    if not os.path.exists(r'D:\programming documents\python\programs\DownloadLuoguProblems\problems'):
+        os.mkdir(r'D:\programming documents\python\programs\DownloadLuoguProblems\problems')
+    main()
