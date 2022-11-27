e5d61c2f3100cb592c908292206466f013b7eeae
Yetong Pang <404060776@qq.com>
2022-11-27 11:41:02 +0800

    Symplify the program

--- main.py
+++ main.py
@@ -10,34 +10,17 @@ with open('path.txt', 'r') as path:
 if not os.path.exists(SavePath):
     os.makedirs(SavePath)
 os.chdir(SavePath)
+problem_list = ['P', 'B', 'CF', 'SP', 'AT', 'uVA']
+problem_list_name = ['主题库', '入门题库', 'CF题库', 'SPOJ题库', 'AtCoder题库', 'UVA题库']
 
 
 def main():
     print("即将开始爬取")
-    print("准备爬取主题库")
-    if not web_crawler.mainProblems():
-        return
-    print("主题库爬取完毕")
-    print("准备爬取入门题库")
-    if not web_crawler.BasicProblems():
-        return
-    print("入门题库爬取完毕")
-    print("准备爬取CF题库")
-    if not web_crawler.CFProblems():
-        return
-    print("CF题库爬取完毕")
-    print("准备爬取SPOJ题库")
-    if not web_crawler.SPOJProblems():
-        return
-    print("SPOJ题库爬取完毕")
-    print("准备爬取AtCoder题库")
-    if not web_crawler.ATProblems():
-        return
-    print("Atcoder题库爬取完毕")
-    print("准备爬取UVA题库")
-    if not web_crawler.UVAProblems():
-        return
-    print("UVA题库爬取完毕")
+    for i, j in zip(problem_list, problem_list_name):
+        print("准备爬取"+j)
+        if not web_crawler.Problems(i):
+            return
+        print(j+"爬取完毕")
 
 
 if __name__ == '__main__':
@@ -45,22 +28,8 @@ if __name__ == '__main__':
         os.mkdir(r'.\problems')
     if not os.path.exists(r'.\.done'):
         os.mkdir(r'.\.done')
-    if not os.path.exists(r'.\.done\P.txt'):
-        a = open(r".\.done\P.txt", "a")
-        a.close()
-    if not os.path.exists(r'.\.done\B.txt'):
-        a = open(r".\.done\B.txt", "a")
-        a.close()
-    if not os.path.exists(r'.\.done\CF.txt'):
-        a = open(r".\.done\CF.txt", "a")
-        a.close()
-    if not os.path.exists(r'.\.done\SP.txt'):
-        a = open(r".\.done\SP.txt", "a")
-        a.close()
-    if not os.path.exists(r'.\.done\AT.txt'):
-        a = open(r".\.done\AT.txt", "a")
-        a.close()
-    if not os.path.exists(r'.\.done\UVA.txt'):
-        a = open(r".\.done\UVA.txt", "a")
-        a.close()
+    for i in problem_list:
+        if not os.path.exists('.\\.done\\'+i+'.txt'):
+            a = open('.\\.done\\'+i+'.txt', "a")
+            a.close()
     main()
